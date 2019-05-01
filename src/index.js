c0b9b3ae305f58427e2a7704c26e9b9e202751a4
Jascha <jascha@jaeh.at>
2019-05-01 19:06:24 +0200

    use GitBadges

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const ThemeList = () =>
         Link({ to: 'https://github.com/magic-themes/docs' }, '@magic-themes/docs'),
       ),
       div('the @magic-themes/docs theme is used in all @magic documentation pages.'),
+      GitBadges({
+        project: 'magic-themes/docs',
+        appveyor: 'jaeh/docs',
+      }),
       Link({ to: 'https://magic-themes.github.io/docs' }, 'docs / demo'),
     ]),
   ])
@@ -17,6 +21,10 @@ ThemeList.style = {
       lineHeight: 1.8,
     },
   },
+}
+
+ThemeList.dependencies = {
+  GitBadges: require('@magic-modules/gitbadges'),
 }
 
 module.exports = ThemeList
