b3144789f78502b5d2c76a59e9471ea56682a5ee
Jascha <jascha@jaeh.at>
2019-12-26 03:36:54 +0100

    update tests

--- test/spec.mjs
+++ test/spec.mjs
@@ -3,14 +3,4 @@ import * as ThemeList from '../src/index.mjs'
 
 export default [
   { fn: () => ThemeList.View, expect: is.function, info: 'expect ThemeList to be a function' },
-  {
-    fn: () => ThemeList.propTypes,
-    expect: is.object,
-    info: 'expect ThemeList to have propTypes object',
-  },
-  {
-    fn: () => ThemeList.propTypes.ThemeList,
-    expect: is.array,
-    info: 'expect ThemeList to have propTypes.ThemeList array',
-  },
 ]
